59bda4497ab0d48993ef9ddcfd1df659c6511a35
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-09-26 23:37:24 +0300

    refactoring

--- person.cpp
+++ person.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+#include <string>
 
 
 int main() {
     int age;
     std::string name;
 
-    std:: cout << "Enter your name: " << std::endl;
+    std:: cout << "Enter your name: ";
     std::getline(std::cin, name);
 
-    std::cout << "Enter your age: " << std::endl;
+    std::cout << "Enter your age: ";
     std::cin >> age;
 
     std::cout << "Your name is " << name << " and your age is " << age << "." << std::endl;
