4ce304dc3b6656174d19d3d61c8eb334426bfc18
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-09-27 21:57:52 +0300

    added numSystems file

---
+++ numSystems.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+int main() {
+    int num1 = 15;
+    int num2 = 017;
+    int num3 = 0x0f;
+    int num4 = 0b00001111;
+
+    std::cout << "Number Systems" << std::endl;
+
+    std::cout << num1 << std::endl;
+    std::cout << num2 << std::endl;
+    std::cout << num3 << std::endl;
+    std::cout << num4 << std::endl;
+}
