45dbce87f663ca346672eb4eb3f2b6c0b20ecb10
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-11-05 16:24:04 +0200

    added ternary.cpp file

---
+++ branching/ternary.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+int main() {
+    int a = 11;
+    int b = 22;
+
+    int max_number = (a >= b) ? a : b; // example of ternary operator usage
+
+    std::cout << "Printing maximum number is: " << max_number << std::endl;
+}
