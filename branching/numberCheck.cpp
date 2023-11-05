a5670ef4b59199ecaa0b93ee3e021b3379bf517a
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-11-05 16:45:29 +0200

    added numberCheck.cpp file

---
+++ branching/numberCheck.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+int main() {
+    int my_number;
+
+    std::cout << "Please type in an integral value: " << std::endl;
+    std::cin >> my_number;
+
+    if (my_number % 2 == 0) {
+        std::cout << my_number << " is even." << std::endl;
+    } else {
+        std::cout << my_number << " is odd." << std::endl;
+    }
+}
