5d65d2670b78d07cba5e4d84d39f871b75eed60f
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-11-05 14:51:41 +0200

    added switch.cpp file to branching folder

---
+++ branching/switch.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+int main() {
+    int tool = 1; // here we can configure what tool to use for example
+
+    switch (tool) {
+        case 1: {
+            std::cout << "Active tool is Pen." << std::endl;
+        }
+        break;
+
+        case 2: {
+            std::cout << "Active too is Marker." << std::endl;
+        }
+        break;
+
+        default: {
+            std::cout << "Can't match any tool." << std::endl;
+        }
+    }
+}
