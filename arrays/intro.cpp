2afb986ac6462e7b003f7db17e76d5ea00d1a184
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-11-15 21:39:36 +0200

    added arrays/intro.cpp module

--- arrays/intro.cpp
+++ arrays/intro.cpp
@@ -4,6 +4,10 @@
 int main() {
     int scores [10];  // each index stores junk data
 
+    scores[0] = 11;
+    scores[4] = 31;
+    scores[8] = 8983;
+
     std::cout << "--- Reading from array ---" << std::endl;
     std::cout << "scores[0] - " << scores[0] << std::endl;
     std::cout << "scores[4] - " << scores[4] << std::endl;
