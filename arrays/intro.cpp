5e955c095b306e82858419a118a33d40fcdf1b1c
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-11-15 21:37:17 +0200

    added arrays folder

---
+++ arrays/intro.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int main() {
+    int scores [10];  // each index stores junk data
+
+    std::cout << "--- Reading from array ---" << std::endl;
+    std::cout << "scores[0] - " << scores[0] << std::endl;
+    std::cout << "scores[4] - " << scores[4] << std::endl;
+    std::cout << "scores[8] - " << scores[8] << std::endl;
+
+    return 0;
+}
