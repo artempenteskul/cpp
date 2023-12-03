9e554bf0c9c931eb0f642f97e3d3ad19b727e233
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-12-03 15:53:38 +0200

    added explanation

--- pointers/charPointers.cpp
+++ pointers/charPointers.cpp
@@ -3,8 +3,9 @@
 
 int main() {
     const char* message {"Hello, World!"};
-    std::cout << "message: " << message << std::endl;
-    std::cout << "message: " << &message << std::endl;
+    std::cout << "message: " << message << std::endl; // prints Hello, World!
+    std::cout << "message: " << *message << std::endl; // prints H
+    std::cout << "message: " << &message << std::endl; // prints address of pointer variable
 
     return 0;
 }
